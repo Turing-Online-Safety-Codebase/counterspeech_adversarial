3678e9a05883248742c9d2074abab7283372b210
Yi-Ling Chung <ychung@turing.ac.uk>
2022-11-18 15:22:26 +0000

    added evaluation for generated text

--- src/evaluation/compute_metrics.py
+++ src/evaluation/compute_metrics.py
@@ -4,6 +4,7 @@
 import argparse
 import pandas
 import logging
+import evaluate
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
@@ -24,12 +25,41 @@ def parse_args():
         print(f"{arg} is {getattr(pars_args, arg)}")
     return pars_args
 
-def plot_confusion_matrix(truth, prediction, title='confusion metric', normalise='true'):
+def compute_classification(true, pred, target_names=["not_hateful", "hateful"]):
+    """Computes the number of votes received for each class labelled for an entry.
+        Three classes are included: disagree, agree, other.
+    Args:
+        true (list): ground truth (correct) target values
+        pred (list): model prediction
+        target_names (list): display names matching the labels (same order)
+    Returns:
+        a dictionary of the evaluation results including accuracy, f1, precision, recall,
+        and confusion metric
+    """
+    results = {}
+    results['accuracy'] = accuracy_score(true, pred)
+    results['precision'] = precision_score(true, pred, average='weighted', zero_division=0)
+    results['recall'] = recall_score(true, pred, average='weighted', zero_division=0)
+    results['f1'] = f1_score(true, pred, average='weighted', zero_division=0)
+    print(f'--confusion metric-- \n {confusion_matrix(true, pred)}')
+    results['tn, fp, fn, tp'] = confusion_matrix(true, pred, normalize='true').ravel()
+    print("\n--full report--")
+    print(classification_report(true, pred, output_dict=False, target_names=target_names))
+    return results
+
+def plot_confusion_matrix(true_labels, prediction, title='confusion metric', normalise='true'):
+    """Plot confusion matrix give true labels and prediction
+        Three classes are included: disagree, agree, other.
+    Args:
+        true_labels (list): ground truth (correct) target values
+        pred (list): model prediction
+        target_names (list): display names matching the labels (same order)
+    """
     ax= plt.subplot()
-    matrix = confusion_matrix(truth, prediction, labels=label)    
+    matrix = confusion_matrix(true_labels, prediction, labels=label)    
     group_counts = [str(value) for value in matrix.flatten()]        
     
-    matrix_p = confusion_matrix(truth, prediction, labels=label, normalize=normalise)
+    matrix_p = confusion_matrix(true_labels, prediction, labels=label, normalize=normalise)
     group_percentages = [round(value, 2) for value in matrix_p.flatten()]
     labels = [f"{v1}\n{v2}" for v1, v2 in zip(group_percentages, group_counts)]
     labels = np.asarray(labels).reshape(3,3)    
@@ -40,30 +70,44 @@ def plot_confusion_matrix(truth, prediction, title='confusion metric', normalise
     ax.set_ylabel('Gold label')
     ax.set_title(title)
 
-def main(df_gold, df_pred, output_path):
+def evalute_classification(df_true, df_pred, output_path):
     logger.setLevel(logging.DEBUG)
     handler = logging.FileHandler(f"{output_path}.log")
-    format = logging.Formatter('%(asctime)s  %(name)s %(levelname)s: %(message)s')
-    handler.setFormatter(format)
+    log_format = logging.Formatter('%(asctime)s  %(name)s %(levelname)s: %(message)s')
+    handler.setFormatter(log_format)
     logger.addHandler(handler)
 
-    y_true = df_gold['label']
+    y_true = df_true['label']
     y_pred = df_pred['label']
 
-    results = {}
-    results['accuracy'] = accuracy_score(y_true, y_pred)
-    results['f1'] = f1_score(y_true, y_pred, average='macro', zero_division = 0)
-    results['precision'] = precision_score(y_true, y_pred, average = 'macro', zero_division = 0)
-    results['recall'] = recall_score( y_true, y_pred, average = 'macro', zero_division = 0)
-    results['cm'] = confusion_matrix(y_true, y_pred, labels=label, normalize='true').ravel()    #tn, fp, fn, tp
+    return compute_classification(y_true, y_pred, target_names=["agree_with_the_posts", "disagree_with_the_posts", "other"])
 
-    for key in results.keys():
-        logger.info(f"Result for {key}: {results[key]}")
-    plot_confusion_matrix(y_true, y_pred)
+def evalute_generation(predictions, references):
+    """Computes evaluation for generated text.
+    Args:
+        predictions (list): ground truth (correct) target values
+        references (list): model prediction
+    Returns:
+        a dictionary of the evaluation results including bleu, rouge, mauve, and bleurt
+    """
+    # TODO: add vocav_size, check the type of prediction and references (for rouge and bleu)
+    # predictions = ["hello there general kenobi","foo bar foobar"]
+    # references = [["hello there general kenobi"], ["foo bar foobar"],] or multiple references = [[["hello there general kenobi"], ["hello there!"]], [["foo bar foobar"]]]
+    
+    results = {}
+    bleu = evaluate.load("bleu")
+    rouge = evaluate.load('rouge')
+    mauve = evaluate.load('mauve')
+    bleurt = evaluate.load("bleurt", module_type="metric", checkpoint="bleurt-base-128")
+
+    results['bleu_results'] = bleu.compute(predictions=predictions, references=references)
+    results['rouge_results'] = rouge.compute(predictions=predictions, references=references)
+    results['bleurt_results'] = bleurt.compute(predictions=predictions, references=references)
+    results['mauve_results'] = mauve.compute(predictions=predictions, references=references)
     return results
 
 if __name__ == "__main__":
     args = parse_args()
     df_gold = pandas.read_csv(args.gold_data_path)
     df_pred = pandas.read_csv(args.pred_data_path)
-    main(df_gold, df_pred, args.output_path)
+    evalute_classification(df_gold, df_pred, args.output_path)
