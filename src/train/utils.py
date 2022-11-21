686b65aa150fa1118ba44a19d25575b339872c4c
Yi-Ling Chung <ychung@turing.ac.uk>
2022-11-21 17:30:04 +0000

    added prompt learning for classification task

---
+++ src/train/utils.py
@@ -0,0 +1,36 @@
+import json
+import pandas
+from sklearn.utils import shuffle
+
+def convert_labels(df):
+    """Converts string labels to integers.
+    Args:
+        df (pd.Dataframe): Input dataframe.
+    Returns:
+        pd.DataFrame: Output dataframe with label columns.
+        int: Number of unique classes.
+    """
+    # Replace label column with int values
+    df['label'] = pandas.Categorical(df['label']).codes
+    n_classes = len(df['label'].unique())
+    return df, n_classes
+
+def load_balanced_n_samples(data_dir, task, split, n_entries):
+    """Loads balanced first n entries of training dataset split across 2 classes.
+    Args:
+        data_dir (str): Directory with data.
+        task (str): Task name e.g. abuse
+        split (str): Split from [train, test, dev] to be sampled from.
+        n_entries (int): Number of entries to sample in total.
+    Returns:
+        pd.DataFrame: Dataset of n rows.
+    """
+    SEED = 123
+    balanced_n = int(n_entries/2)
+    df = pandas.read_csv(f'{data_dir}/{task}/clean_data/{task}_{split}.csv')
+    df_agree = df[df['label'] == 0].head(balanced_n)
+    df_disagree = df[df['label'] == 1].head(balanced_n)
+    df_other = df[df['label'] == 2].head(balanced_n)
+    df_concat = pandas.concat([df_agree, df_disagree, df_other])
+    shuffled_df = shuffle(df_concat, random_state = SEED)
+    return shuffled_df
