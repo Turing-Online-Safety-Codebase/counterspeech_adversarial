c51a01d51a8740a0036d7454e209f57d81d036dd
Ubuntu <onlinesafety@turing.ac.uk>
2023-10-02 08:33:35 +0000

    updated test script for s2slm

---
+++ src/evaluation/cs_generation_test_s2slm.py
@@ -0,0 +1,321 @@
+#!/usr/bin/env python3
+# coding=utf-8
+# Copyright 2018 Google AI, Google Brain and Carnegie Mellon University Authors and the HuggingFace Inc. team.
+# Copyright (c) 2018, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+""" 
+This script is adapted from run_generation.py from HuggingFace for counter speech generation.
+Conditional text generation with the auto-regressive models of the library (GPT/GPT-2/CTRL/Transformer-XL/XLNet)
+"""
+
+
+import argparse
+import logging
+
+import numpy as np
+import torch
+import datetime
+import pandas
+from transformers import AutoTokenizer, AutoConfig, AutoModelForCausalLM, AutoModelForSequenceClassification, AutoModelForSeq2SeqLM
+from transformers import (
+    CTRLLMHeadModel,
+    CTRLTokenizer,
+    GPT2LMHeadModel,
+    GPT2Tokenizer,
+    OpenAIGPTLMHeadModel,
+    OpenAIGPTTokenizer,
+    TransfoXLLMHeadModel,
+    TransfoXLTokenizer,
+    XLMTokenizer,
+    XLMWithLMHeadModel,
+    XLNetLMHeadModel,
+    XLNetTokenizer,
+)
+
+
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
+    datefmt="%m/%d/%Y %H:%M:%S",
+    level=logging.INFO,
+)
+logger = logging.getLogger(__name__)
+
+MAX_LENGTH = int(10000)  # Hardcoded max length to avoid infinite loop
+
+MODEL_CLASSES = {
+    "gpt2": (GPT2LMHeadModel, GPT2Tokenizer),
+    "ctrl": (CTRLLMHeadModel, CTRLTokenizer),
+    "openai-gpt": (OpenAIGPTLMHeadModel, OpenAIGPTTokenizer),
+    "xlnet": (XLNetLMHeadModel, XLNetTokenizer),
+    "transfo-xl": (TransfoXLLMHeadModel, TransfoXLTokenizer),
+    "xlm": (XLMWithLMHeadModel, XLMTokenizer),
+}
+
+# MODEL_CONFIG_CLASSES = list(MODEL_FOR_CAUSAL_LM_MAPPING.keys())
+# MODEL_TYPES = tuple(conf.model_type for conf in MODEL_CONFIG_CLASSES)
+
+# Padding text to help Transformer-XL and XLNet with short prompts as proposed by Aman Rusia
+# in https://github.com/rusiaaman/XLNet-gen#methodology
+# and https://medium.com/@amanrusia/xlnet-speaks-comparison-to-gpt-2-ea1a4e9ba39e
+PREFIX = """In 1991, the remains of Russian Tsar Nicholas II and his family
+(except for Alexei and Maria) are discovered.
+The voice of Nicholas's young son, Tsarevich Alexei Nikolaevich, narrates the
+remainder of the story. 1883 Western Siberia,
+a young Grigori Rasputin is asked by his father and a group of men to perform magic.
+Rasputin has a vision and denounces one of the men as a horse thief. Although his
+father initially slaps him for making such an accusation, Rasputin watches as the
+man is chased outside and beaten. Twenty years later, Rasputin sees a vision of
+the Virgin Mary, prompting him to become a priest. Rasputin quickly becomes famous,
+with people, even a bishop, begging for his blessing. <eod> </s> <eos>"""
+
+
+def set_seed(args):
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    if args.n_gpu > 0:
+        torch.cuda.manual_seed_all(args.seed)
+
+
+#
+# Functions to prepare models' input
+#
+
+
+def prepare_ctrl_input(args, _, tokenizer, prompt_text):
+    if args.temperature > 0.7:
+        logger.info("CTRL typically works better with lower temperatures (and lower top_k).")
+
+    encoded_prompt = tokenizer.encode(prompt_text, add_special_tokens=False)
+    if not any(encoded_prompt[0] == x for x in tokenizer.control_codes.values()):
+        logger.info("WARNING! You are not starting your generation from a control code so you won't get good results")
+    return prompt_text
+
+
+def prepare_xlm_input(args, model, tokenizer, prompt_text):
+    # kwargs = {"language": None, "mask_token_id": None}
+
+    # Set the language
+    use_lang_emb = hasattr(model.config, "use_lang_emb") and model.config.use_lang_emb
+    if hasattr(model.config, "lang2id") and use_lang_emb:
+        available_languages = model.config.lang2id.keys()
+        if args.xlm_language in available_languages:
+            language = args.xlm_language
+        else:
+            language = None
+            while language not in available_languages:
+                language = input("Using XLM. Select language in " + str(list(available_languages)) + " >>> ")
+
+        model.config.lang_id = model.config.lang2id[language]
+        # kwargs["language"] = tokenizer.lang2id[language]
+    return prompt_text
+
+
+def prepare_xlnet_input(args, _, tokenizer, prompt_text):
+    prefix = args.prefix if args.prefix else args.padding_text if args.padding_text else PREFIX
+    prompt_text = prefix + prompt_text
+    return prompt_text
+
+
+def prepare_transfoxl_input(args, _, tokenizer, prompt_text):
+    prefix = args.prefix if args.prefix else args.padding_text if args.padding_text else PREFIX
+    prompt_text = prefix + prompt_text
+    return prompt_text
+
+
+PREPROCESSING_FUNCTIONS = {
+    "ctrl": prepare_ctrl_input,
+    "xlm": prepare_xlm_input,
+    "xlnet": prepare_xlnet_input,
+    "transfo-xl": prepare_transfoxl_input,
+}
+
+
+def adjust_length_to_model(length, max_sequence_length):
+    if length < 0 and max_sequence_length > 0:
+        length = max_sequence_length
+    elif 0 < max_sequence_length < length:
+        length = max_sequence_length  # No generation bigger than model size
+    elif length < 0:
+        length = MAX_LENGTH  # avoid infinite loop
+    return length
+
+def abuse_filter(abusive_text, sequence, df, regeneration_needed):
+    # TODO: a classifier that can classify if a generated text is abusive 
+    # To be used in the generation pipeline
+    model_name = "experiments/models/iter_0/run1_final_deberta"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSequenceClassification.from_pretrained(model_name)
+    label_dict = {0: 'agrees_with_the_post', 1: 'disagrees_with_the_post', 2: 'other'}
+
+    predict = tokenizer(abusive_text, sequence, return_tensors="pt")
+    logits = model(**predict).logits
+    scores = torch.softmax(logits, dim=1)
+    pred_prob_list = scores.tolist()[0]
+    pred_indice = scores.argmax().item()
+
+    if pred_indice == 0:
+        regeneration_needed = True
+        print("It is abuse. Regeneration is needed")
+        # print(f"Confidence counter speech is {int(round(pred_prob_list[1] * 100))}%.\nConfidence not_counter_speech is {int(round(pred_prob_list[0] * 100))}%\nConfidence other is {int(round(pred_prob_list[2] * 100))}%")
+    else:
+        regeneration_needed = False
+    # append data
+    new_row = {'abusive_speech':abusive_text, 'response': sequence, 'pred_label': pred_indice, 'pred_prob': pred_prob_list[pred_indice]}
+    df1 = df.append(new_row, ignore_index=True)
+    return regeneration_needed, df
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_type", default=None, type=str, required=True, help="Model type selected in the list: " + ", ".join(MODEL_CLASSES.keys()))
+    parser.add_argument("--model_name_or_path", default=None, type=str, required=True, help="Path to pre-trained model or shortcut name selected in the list: " + ", ".join(MODEL_CLASSES.keys()))
+
+    # added
+    parser.add_argument("--test_file", type=str, default="")
+    parser.add_argument("--output_raw_file_path", type=str, default="")
+    parser.add_argument("--output_clean_file_path", type=str, default="")
+    parser.add_argument("--prompt", type=str, default="")
+    parser.add_argument("--length", type=int, default=20)
+    parser.add_argument("--stop_token", type=str, default=None, help="Token at which text generation is stopped")
+
+    parser.add_argument("--temperature", type=float, default=1.0, help="temperature of 1.0 has no effect, lower tend toward greedy sampling")
+    parser.add_argument("--repetition_penalty", type=float, default=1.0, help="primarily useful for CTRL model; in that case, use 1.2")
+    parser.add_argument("--k", type=int, default=0)
+    parser.add_argument("--p", type=float, default=0.9)
+
+    parser.add_argument("--prefix", type=str, default="", help="Text added prior to input.")
+    parser.add_argument("--padding_text", type=str, default="", help="Deprecated, the use of `--prefix` is preferred.")
+    parser.add_argument("--xlm_language", type=str, default="", help="Optional language when used with the XLM model.")
+
+    parser.add_argument("--seed", type=int, default=42, help="random seed for initialization")
+    parser.add_argument("--no_cuda", action="store_true", help="Avoid using CUDA when available")
+    parser.add_argument("--num_return_sequences", type=int, default=1, help="The number of samples to generate.")
+    parser.add_argument("--fp16", action="store_true", help="Whether to use 16-bit (mixed) precision (through NVIDIA apex) instead of 32-bit")
+    parser.add_argument("--seq2seqlm", action="store_true", help="If using sequence-to-sequence LMs. Default to False.")
+    args = parser.parse_args()
+
+    args.device = torch.device("cuda" if torch.cuda.is_available() and not args.no_cuda else "cpu")
+    args.n_gpu = 0 if args.no_cuda else torch.cuda.device_count()
+
+    logger.warning(f"device: {args.device}, n_gpu: {args.n_gpu}, 16-bits training: {args.fp16}")
+
+    set_seed(args)
+
+    # Initialize the model and tokenizer
+    config = AutoConfig.from_pretrained(args.model_name_or_path)
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path)
+
+    if args.seq2seqlm:
+        model = AutoModelForSeq2SeqLM.from_pretrained(
+            args.model_name_or_path,
+            # from_tf=bool(".ckpt" in args.model_name_or_path),
+            # config=config,
+            )
+    else:
+        model = AutoModelForCausalLM.from_pretrained(
+            args.model_name_or_path,
+            from_tf=bool(".ckpt" in args.model_name_or_path),
+            config=config,
+        )
+    # model.to(args.device)
+
+    if args.fp16:
+        model.half()
+
+    # args.length = adjust_length_to_model(args.length, max_sequence_length=model.config.max_position_embeddings)
+    logger.info(args)
+
+    # create a dataframe containing generaion text that is abusive
+    # df_generation = pandas.DataFrame({'abusive_speech':[], 'response': [], 'pred_label': [], 'pred_prob': []})
+
+    output_raw_file = open(args.output_raw_file_path, "w")
+    output_clean_file = open(args.output_clean_file_path, "w")
+    with open(args.test_file) as test_f:
+        for line in test_f:
+            text = ""
+            prompt_text = line.replace("\n", "")
+
+            # Different models need different input formatting and/or extra arguments
+            requires_preprocessing = args.model_type in PREPROCESSING_FUNCTIONS.keys()
+            if requires_preprocessing:
+                prepare_input = PREPROCESSING_FUNCTIONS.get(args.model_type)
+                preprocessed_prompt_text = prepare_input(args, model, tokenizer, prompt_text)
+
+                if model.__class__.__name__ in ["TransfoXLLMHeadModel"]:
+                    tokenizer_kwargs = {"add_space_before_punct_symbol": True}
+                else:
+                    tokenizer_kwargs = {}
+
+                encoded_prompt = tokenizer.encode(
+                    preprocessed_prompt_text, add_special_tokens=False, return_tensors="pt", **tokenizer_kwargs
+                )
+            else:
+                # prefix = args.prefix if args.prefix else args.padding_text
+                # encoded_prompt = tokenizer.encode(prefix + prompt_text, add_special_tokens=False, return_tensors="pt")
+                # encoded_prompt = tokenizer.encode(prompt_text, add_special_tokens=True, return_tensors="pt")
+                prefix = " response: " if args.seq2seqlm else ''
+                # encoded_prompt = tokenizer.encode(prompt_text + suffix, add_special_tokens=True, return_tensors="pt")
+                encoded_prompt = tokenizer([prefix + prompt_text], max_length=256, truncation=True, return_tensors="pt")
+            # encoded_prompt = encoded_prompt.to(args.device)
+
+            # num_attempts = 0
+            # TODO: setup a variable for whether to regenerate for poor generation
+            # regenerate = True
+
+            # TODO: inject an abuse classifier in the pipeline
+            # Set this abuse classifier option as a parameter that can be optional
+            # num_attempts += 1
+
+            output_sequences = model.generate(
+                **encoded_prompt,
+                temperature=args.temperature,
+                top_p=args.p,
+                do_sample=True,
+                min_length=10,
+                max_length=128,
+            )
+
+            # Decode text
+            decoded_output = tokenizer.batch_decode(output_sequences, skip_special_tokens=True)[0]
+
+            # Remove all text after the stop token
+            # text = text[: text.find(args.stop_token) if args.stop_token else None]
+
+            # Add the prompt at the beginning of the sequence. Remove the excess text that was used for pre-processing
+            # total_sequence = (
+            #     prompt_text + text[len(tokenizer.decode(encoded_prompt[0], clean_up_tokenization_spaces=True)) :]
+            # )
+            # output = text.replace(prompt_text, "")
+            # clean_output = output.split(' <END>')[0].replace('</s>', '').replace('<pad>', '').strip()
+
+            # generated_sequences.append(total_sequence)
+            # print("HS:", prompt_text)
+            # print("CS:", decoded_output)
+            # print("CO:", clean_output)
+
+            # if "<END>" in output or num_attempts > 3:
+            #     break
+            # regenerate, df_generation = abuse_filter(prompt_text, clean_output, df_generation)
+
+            # print("====== New Entry======")
+            output_raw_file.write("====== New Entry======" + "\n")
+            output_raw_file.write(prefix + prompt_text + ' <response> ' + decoded_output + "\n")
+            
+            output_clean_file.write(decoded_output + "\n")
+
+    output_raw_file.close()
+    output_clean_file.close()
+
+if __name__ == "__main__":
+    main()
